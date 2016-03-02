26e62c56b352e795fdf0afa50544fb4442821e77
Richard Ma <richard.ma.19850509@gmail.com>
2016-03-02 12:01:20 +0800

    remove org-mode init setting

--- init.el
+++ init.el
@@ -87,7 +87,7 @@
 (require 'init-erlang)
 (require 'init-javascript)
 (require 'init-php)
-(require 'init-org)
+;;(require 'init-org)
 (require 'init-nxml)
 (require 'init-html)
 (require 'init-css)
--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,5 +1,1 @@
-;; set ibus input method
-(require 'ibus)
-(add-hook 'after-init-hook 'ibus-mode-on)
-
 (provide 'init-local)
